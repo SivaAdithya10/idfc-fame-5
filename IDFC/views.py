04adc65371a2dc885793754eeee737bc6a2e6f00
Sivaadithya <sivaadithyasripathi@gmail.com>
2025-08-31 12:37:28 +0530

    Frontend ready. Working on Agents v2

--- IDFC/views.py
+++ IDFC/views.py
@@ -1,14 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.conf import settings
-import os
-import markdown
 
 def index(request):
     return render(request, 'index.html')
-
-# def transactions_view(request):
-#     return HttpResponse("This is the transactions page.")
-
-# def instructions_view(request):
-#     return render(request, 'index.html')
